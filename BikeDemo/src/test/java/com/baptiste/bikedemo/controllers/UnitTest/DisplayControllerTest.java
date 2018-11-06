f0442436144856b960834f67f788b47872ef8831
riley.baptiste <riley.baptiste@OKCL007.ruralsource.com>
2018-11-06 14:53:30 -0600

    Repository Integration testing..

--- BikeDemo/src/test/java/com/baptiste/bikedemo/controllers/UnitTest/DisplayControllerTest.java
+++ BikeDemo/src/test/java/com/baptiste/bikedemo/controllers/UnitTest/DisplayControllerTest.java
@@ -1,9 +1,11 @@
 package com.baptiste.bikedemo.controllers.UnitTest;
 
-import static org.junit.Assert.fail;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.math.BigDecimal;
+import java.util.Date;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -13,17 +15,19 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import com.baptiste.bikedemo.controllers.DisplayController;
+import com.baptiste.bikedemo.model.Bike;
 import com.baptiste.bikedemo.service.BikeService;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(DisplayControllerTest.class)
-public class DisplayControllerTest {
+public class DisplayControllerTest  {
 
 	@Autowired
 	private MockMvc mockMvc;
@@ -48,13 +52,32 @@ public class DisplayControllerTest {
 	}
 
 	@Test
-	public void testGetEvent() {
-		fail("Not yet implemented");
+	@WithMockUser(username = "riley", password = "test", roles = { "USER" })
+	public void testGetEvent() throws Exception {
+		// Mocking the Service saving the bike..
+		Bike mockBike = new Bike();
+
+		mockBike.setEmail("bjackson@yahoo.com");
+		mockBike.setName("Bobby Jackson");
+		mockBike.setPurchasePrice(new BigDecimal(508.23));
+		mockBike.setSerialNumber("SN9876543210");
+		mockBike.setPhone("405-448-8747");
+		mockBike.setModel("XM300");
+		mockBike.setPurchaseDate(new Date());
+
+		
+		
+		when(bikeService.saveBike(mockBike)).thenReturn(true);
+		mockMvc.perform(get("/Display/3")).andExpect(status().isOk()).andReturn();
 	}
 
 	@Test
-	public void testPageViewOneByOne() {
-		fail("Not yet implemented");
+	public void testPageViewOneByOne() throws Exception {
+		
+		Page<Bike> pp = null; 		
+		when(bikeService.getBike(5)).thenReturn(pp);
+		mockMvc.perform(get("/Display/Page?p=3")).andExpect(status().isOk()).andReturn();
+		
 	}
 
 }
---
+++ BikeDemo/src/test/java/com/baptiste/bikedemo/repository/BikeRepositoryTest.java
@@ -0,0 +1,75 @@
+package com.baptiste.bikedemo.repository;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.baptiste.bikedemo.model.Bike;
+import com.baptiste.bikedemo.service.BikeService;
+
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace=Replace.NONE)
+public class BikeRepositoryTest {
+
+
+	@Autowired
+	TestEntityManager em;
+
+	@Autowired
+	BikeRepository bikeRepository;
+	
+
+
+	@Test
+	public void testFindAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindAllByIdIterableOfID() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetOne() {
+	
+
+		Long id = 8L;
+		Bike bike = new Bike();
+		bike.setId(id);
+
+		bike.setEmail("bjackson@yahoo.com");
+		bike.setName("Bobby Jackson");
+		bike.setPurchasePrice(new BigDecimal(508.23));
+		bike.setSerialNumber("SN9876543210");
+		bike.setPhone("405-448-8747");
+		bike.setModel("XM300");
+		bike.setPurchaseDate(new Date());
+
+		
+		//em.persist(bike);
+
+		Optional<Bike> foundBike = bikeRepository.findById(id);
+				
+		assertThat(foundBike.get().getId(), is(equalTo(id)));
+
+	}
+
+}
